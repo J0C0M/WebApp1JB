128c86b4f69d468667bc02e824bed8a6657382ef
J0C0M <117586494+J0C0M@users.noreply.github.com>
2024-04-10 10:26:08 +0200

    html push

--- index.php
+++ index.php
@@ -3,22 +3,33 @@
     <head>
         <title>Document</title>
         <link rel="stylesheet" href="css/pannenkoeken.css">
+        <link href="https://fonts.cdnfonts.com/css/harlow-solid-italic" rel="stylesheet">
     </head>
     <body>
         <header>
-            <img src="images/faardyy.png" class="header-img">
+            <img src="images/faardyy.png" class="header-img header-img-pos">
+            <div class="header-overlapper header-box"></div>
             <div class="header-overlapper">
-                <img src="images/DBPlogo-img" class="">
+                
+                <img src="images/fardylogotransparent.png" class="logo-header-img">
                 <div class="header-middle-part">
-
+                        <a class="header-btn" href="index.php">home</a>
+                        <a class="header-btn" href="index.php">Over Ons</a>
+                        <div class="search-bar-titel">
+                            <h1 class="header-txt">De Betoverde Pannenkoek</h1>
+                        </div>
+                        <a class="header-btn" href="index.php">Menukaart</a>
+                        <a class="header-btn" href="index.php">Contact</a>
                 </div>
                 <div class="header-login">
 
                 </div>
             </div>
         </header>
-    
         <main>
+            <divo class="over-ons-container">
+                <h1>over ons</h1>
+            </div>
         </main>
 
         <footer>
