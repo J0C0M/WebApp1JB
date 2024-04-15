9da82932cc289ed94b6fe4e969967305d8bda100
J0C0M <117586494+J0C0M@users.noreply.github.com>
2024-04-15 22:21:11 +0200

    connection change

--- index.php
+++ index.php
@@ -23,7 +23,6 @@
                         <a class="header-btn" href="login.php">Contact</a>
                 </div>
                 <div class="header-login">
-
                 </div>  
             </div>
         </header>
@@ -33,9 +32,30 @@
                 <div class=""></div>
                 <div></div>
             </div>
-        </main>
 
+        </main>
+        
         <footer>
         </footer>
     </body>
 </html>
+
+<?php
+$host = 'db';
+$db   = 'mydatabase';
+$user = 'user';
+$pass = 'password';
+$charset = 'utf8mb4';
+ 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try{
+    $connection = new PDO ($dsn, $user, $pass, $options);
+    } catch (\PDOExceptions $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
